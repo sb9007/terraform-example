03aed59b06c8ba5f695eaeb14a2ae853269ed2d5
sb9007 <sushainb@gmail.com>
2026-04-03 02:23:39 +0530

    Auto Scaling Group Example added

--- main.tf
+++ main.tf
@@ -1,44 +1,155 @@
-provider "aws" {
-    region = "us-east-2"
-}
+            provider "aws" {
+                region = "us-east-2"
+            }
 
-resource "aws_instance" "example" {
-    ami = "ami-0fb653ca2d3203ac1"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [aws_security_group.instance.id]
+            #Replaced the aws_instance example with Auto Scaling Group Example for Deploying cluste of web servers topic
 
-    user_data = <<-EOF
-                #!/bin/bash
-                echo "Hello, World!" > index.html
-                nohup busybox httpd -f -p ${var.server_port} &
-                EOF
+                resource "aws_launch_template" "example" {
+                    image_id = "ami-0fb653ca2d3203ac1"
+                    instance_type = "t3.micro"
+                    vpc_security_group_ids = [aws_security_group.instance.id]
 
-    user_data_replace_on_change = true
+                    user_data = base64encode(
+                    <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p ${var.server_port} &
+                    EOF
+                    )
 
-    tags = {
-        Name = "terraform-example"
-    }
-}
+                    #Required when using a launch configuration with an auto scaling group.
+                    lifecycle {
+                    create_before_destroy = true
+                }
+                }
 
-resource "aws_security_group" "instance" {
-    name = "terraform-example-instance"
+            resource "aws_autoscaling_group" "example" {
+            launch_template {
+                id      = aws_launch_template.example.id
+                version = "$Latest"
+        }
+                vpc_zone_identifier = data.aws_subnets.default.ids
 
-    ingress {
-        from_port = var.server_port
-        to_port = var.server_port
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
+                target_group_arns = [aws_lb_target_group.asg.arn]
+                health_check_type = "ELB"
 
-variable "server_port" {
-    description = "The port the server will use for HTTP requests"
-    type = number
-    default = 8080
-}
+                min_size = 2
+                max_size = 10
 
-output "public_ip" {
-    value = aws_instance.example.public_ip
-    description = "The public IP address of the web server"
-}
+                tag {
+                    key = "Name"
+                    value = "terraform-asg-example"
+                    propagate_at_launch = true
+                }
+            }
+
+            data "aws_vpc" "default" {
+            default = true
+            }
+
+            data "aws_subnets" "default" {
+            filter {
+                name = "vpc-id"
+                values = [data.aws_vpc.default.id]
+            }
+            }
+
+            resource "aws_lb" "example" {
+            name = "terraform-asg-example"
+            load_balancer_type = "application"
+            subnets = data.aws_subnets.default.ids
+            security_groups = [aws_security_group.alb.id]
+            }
+
+            resource "aws_lb_listener" "http" {
+            load_balancer_arn = aws_lb.example.arn
+            port = 80
+            protocol = "HTTP"
+
+            # By default, return a simple 404 page
+            default_action {
+                type = "fixed-response"
+
+                fixed_response {
+                content_type = "text/plain"
+                message_body = "404: page not found"
+                status_code = 404
+                }
+            }
+            }
+
+            resource "aws_security_group" "alb" {
+            name = "terraform-example-alb"
+
+            # Allow inboung HTTP requests
+            ingress {
+                from_port = 80
+                to_port = 80
+                protocol = "tcp"
+                cidr_blocks = ["0.0.0.0/0"]
+            }
+
+            # Allow all outbound requests
+            egress {
+                from_port = 0
+                to_port = 0
+                protocol = "-1"
+                cidr_blocks = ["0.0.0.0/0"]
+            }
+            }
+
+            resource "aws_lb_target_group" "asg" {
+            name = "terraform-asg-example"
+            port = var.server_port
+            protocol = "HTTP"
+            vpc_id = data.aws_vpc.default.id
+
+            health_check {
+                path = "/"
+                protocol = "HTTP"
+                matcher = "200"
+                interval = 15
+                timeout = 3
+                healthy_threshold = 2
+                unhealthy_threshold = 2
+            }
+            }
+
+            resource "aws_lb_listener_rule" "asg" {
+            listener_arn = aws_lb_listener.http.arn
+            priority = 100
+
+            condition {
+                path_pattern {
+                    values = ["*"]
+                }
+            }
+
+            action {
+                type = "forward"
+                target_group_arn = aws_lb_target_group.asg.arn
+            }
+            }
+
+            resource "aws_security_group" "instance" {
+                name = "terraform-example-instance"
+
+                ingress {
+                    from_port = var.server_port
+                    to_port = var.server_port
+                    protocol = "tcp"
+                    cidr_blocks = ["0.0.0.0/0"]
+                }
+            }
+
+            variable "server_port" {
+                description = "The port the server will use for HTTP requests"
+                type = number
+                default = 8080
+            }
+
+            output "alb_dns_name" {
+                value = aws_lb.example.dns_name
+                description = "The domain name of the load balancer"
+            }
 
